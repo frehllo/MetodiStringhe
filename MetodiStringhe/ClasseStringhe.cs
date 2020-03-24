731df439aee1cc58f2a4bad625a0ddafd99bd7f6
narcisbuzatu2003@gmail.com <narcisbuzatu2003@gmail.com>
2020-03-24 15:10:50 +0100

    Aggiungere i file di progetto.

---
+++ MetodiStringhe/ClasseStringhe.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodiStringhe
+{
+    public class ClasseStringhe
+    {
+        public static bool Uguale(string stringa1, string stringa2)
+        {
+            if(stringa1 == stringa2)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+        public static bool ContieneChar(string stringa)
+        {
+            int maiuscole = 0;
+            for (int i = 0; i < stringa.Length; i++)
+            {
+                if(Char.IsUpper(stringa[i]) == true)
+                {
+                    maiuscole++;
+                }
+            }
+            if (maiuscole > 0)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
---
+++ ProvaStringhe/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProvaStringhe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //METODO UGUALI
+            Console.WriteLine("prova Uguali");
+            //scrittura e lettura prima stringa
+            Console.Write("stringa 1: ");
+            string stringa1 = Console.ReadLine();
+            //scrittura e lettura seconda stringa
+            Console.Write("stringa 2: ");
+            string stringa2 = Console.ReadLine();
+            //richiamo il metodo e scrittura risposta
+            bool ris = MetodiStringhe.ClasseStringhe.Uguale(stringa1, stringa2);
+            Console.WriteLine(ris);
+
+            //METODO CONTIENECHAR
+            Console.WriteLine("prova ContieneChar");
+            //scrittura e lettura stringa
+            Console.Write("stringa: ");
+            string stringa = Console.ReadLine();
+            bool ris2 = MetodiStringhe.ClasseStringhe.ContieneChar(stringa);
+            Console.WriteLine(ris2);
+        }
+    }
+}
